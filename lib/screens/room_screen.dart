39fbdd41a02bdaa16a4b9d6cb8d7b77f303b5af5
stinekamau <60697895+stinekamau@users.noreply.github.com>
2021-12-29 03:47:08 +0300

    added individual room screen

--- lib/screens/room_screen.dart
+++ lib/screens/room_screen.dart
@@ -1,89 +1,178 @@
-import 'dart:math';
-
-import 'package:club_house/widgets/user_profile.dart';
+import 'package:club_house/widgets/room_images.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:club_house/widgets/widgets.dart';
+import 'package:club_house/data.dart';
+import 'dart:math';
 
-import '../data.dart';
+class RoomScreen extends StatelessWidget {
+  Random rand = new Random();
+  var checkList = [true, false];
 
-class RoomScreen extends StatelessWidget
-{
-  final Room room;
-  const RoomScreen({Key? key,required this.room}):super(key:key);
+  Room room;
+  RoomScreen({Key? key, required this.room}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
+    return SafeArea(
+      child: Scaffold(
         appBar: AppBar(
+          leadingWidth: 150,
           elevation: 0,
-          leadingWidth: 120,
           leading: TextButton.icon(
-            label: Text("All rooms"),
-            icon: const Icon(CupertinoIcons.chevron_down),
-            onPressed: () { Navigator.of(context).pop(); },
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
             style: TextButton.styleFrom(primary: Colors.black),
+            icon: Icon(CupertinoIcons.chevron_down),
+            label: Text("All Rooms"),
           ),
-
           actions: [
-            IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.doc,size: 28.0,),),
+            IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.doc)),
             GestureDetector(
-              onTap: (){},
+              onTap: () {},
               child: Padding(
-                  padding: EdgeInsets.fromLTRB(4,6,3,6),
-                  child:
-                  UserProfileImage(imageurl: currentUser.imageUrl)
-              ),
+                  padding: EdgeInsets.fromLTRB(4, 6, 3, 6),
+                  child: UserProfileImage(imageurl: currentUser.imageUrl)),
             ),
-
-
           ],
-
         ),
         body: Container(
-          padding: const EdgeInsets.all(20.0),
+          padding: EdgeInsets.all(20.0),
           height: MediaQuery.of(context).size.height,
           width: double.infinity,
           decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(50.0),
-          ),
+              color: Colors.white, borderRadius: BorderRadius.circular(40)),
           child: CustomScrollView(
             slivers: [
               SliverToBoxAdapter(
-                child: Column(children: [
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('${room.club}'.toUpperCase(),
-                          style:Theme.of(context).textTheme.overline!.copyWith(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            letterSpacing: 1.0,
-                          ),
-                        overflow: TextOverflow.fade,
-                      ),
-                    GestureDetector( onTap: (){}, child: Icon(CupertinoIcons.ellipsis)),
-                  ],),
-                  Text(room.name,style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                    fontSize:16.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.0
-                  ),)
-                ],),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(room.club),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(CupertinoIcons.ellipsis))
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Welcome to Clubhouse 🏡\n (Walkthrough with Q&A)",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: 5,
+                    )
+                  ],
+                ),
               ),
               SliverGrid.count(
-                  crossAxisCount: 3,
-                  mainAxisSpacing: 20,
-                  children: room.speakers.map((e)=>RoomUserProfile(imageURL: e.imageUrl,name: e.givenName,size:45,isNew:Random().nextBool(),isMuted: Random().nextBool(),)).toList()
+                crossAxisCount: 3,
+                mainAxisSpacing: 20.0,
+                children: room.speakers
+                    .map((e) => RoomImage(
+                          image: UserProfileImage(
+                            imageurl: e.imageUrl,
+                            size: 50,
+                          ),
+                          readyTalk: rand.nextBool(),
+                          name: e.givenName,
+                        ))
+                    .toList(),
+              ),
+              SliverToBoxAdapter(
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      "Followed by the speakers",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w100, fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                  ],
+                ),
+              ),
+              SliverGrid.count(
+                crossAxisCount: 4,
+                mainAxisSpacing: 20.0,
+                children: room.followedBySpeakers
+                    .map((e) => RoomImage(
+                          micSize: 18,
+                          partySize: 18,
+                          image: UserProfileImage(
+                            imageurl: e.imageUrl,
+                            size: 36,
+                          ),
+                          readyTalk: rand.nextBool(),
+                          name: e.givenName,
+                        ))
+                    .toList(),
+              ),
+              SliverToBoxAdapter(
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      "Other speakers",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w100, fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                  ],
+                ),
+              ),
+              SliverGrid.count(
+                crossAxisCount: 4,
+                mainAxisSpacing: 20.0,
+                children: room.others
+                    .map((e) => RoomImage(
+                          micSize: 18,
+                          partySize: 18,
+                          image: UserProfileImage(
+                            imageurl: e.imageUrl,
+                            size: 36,
+                          ),
+                          readyTalk: rand.nextBool(),
+                          name: e.givenName,
+                        ))
+                    .toList(),
+              ),
+              SliverToBoxAdapter(
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: 5,
+                    )
+                  ],
+                ),
               )
             ],
-
           ),
         ),
-
-      );
+      ),
+    );
   }
 }
