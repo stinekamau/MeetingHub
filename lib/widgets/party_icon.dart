2f1c523bb69a310c3cc5b475ab4804d9a36bbc32
stinekamau <60697895+stinekamau@users.noreply.github.com>
2021-12-29 03:48:12 +0300

    Custom widget for displaying the party Icon

---
+++ lib/widgets/party_icon.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// class PartyIcon extends StatelessWidget {
+//   double partySize;
+//   PartyIcon({Key? key, this.partySize = 17}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(3.0),
+//       child: Container(
+//         width: 100,
+//         height: 100,
+//         decoration: BoxDecoration(
+//           color: Colors.white,
+//           shape: BoxShape.circle,
+//         ),
+//         child: Text(
+//           "🎉",
+//           style: TextStyle(fontSize: partySize),
+//         ),
+//       ),
+//     );
+//   }
+// }
